03edebe66c0eff7fefe9978d3739b4bc3e81ab46
FelipeRamosDev <felipe@feliperamos.info>
2025-05-25 11:11:04 -0300

    [FRCV-5] Cluster Service
    ## [FRCV-5] Description
    Created the service to manage the CLUSTER and its CPUs/Thread under.

--- cluster.ts
+++ cluster.ts
@@ -1,1 +1,9 @@
+import Cluster from './src/services/ClusterManager/Cluster';
 
+new Cluster({
+   tagName: 'feliperamos-cv',
+   cores: [ ],
+   onReady: () => {
+      console.log('Cluster is ready!');
+   }
+});
