7fbc5b52d6125531238e5f0e21b35b41ed9c0735
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-05-25 15:39:17 -0300

    [FRCV-3] Slack App (#8)
    * [FRCV-7] SlackApp Service (#6)
    Created the base for SlackApp instance to manage slack features
    
    * [FRCV-9] SlackApp listeners (#7)
    Created the methods to implement the listeners for message and action

---
+++ src/services/SlackApp/index.ts
@@ -0,0 +1,56 @@
+import { SlackAppSetup } from '../types/SlackApp.types';
+import { App, StringIndexed } from '@slack/bolt';
+
+export default class SlackApp {
+   public app: App;
+
+   constructor (setup: SlackAppSetup) {
+      const { socketMode = true, port = 3000, signingSecret, appToken, botToken, ...options } = setup || {};
+
+      this.app = new App({
+         token: botToken,
+         appToken,
+         signingSecret,
+         socketMode,
+         port,
+         ...options
+      });
+
+      this.app.start().then(() => {
+         this.log('Bolt app is running!');
+      }).catch(err => {
+         this.logError(err);
+      });
+
+      // Declaring the thread on the current process to be used anywhere on on the worker thread.
+      process.slack = this;
+   }
+
+   log(...data: any) {
+      this.app.logger.info('⚡️ ', ...data);
+   }
+
+   logError(...data: any) {
+      this.app.logger.error('⚡️ ', ...data);
+   }
+
+   onMessage(callback: (params: StringIndexed) => Promise<void>, contain?: string) {
+      if (!callback) {
+         return;
+      }
+
+      if (contain) {
+         this.app.message(contain, callback);
+      } else {
+         this.app.message(callback);
+      }
+   }
+
+   onAction(actionID: string, callback: (params: StringIndexed) => Promise<void>) {
+      if (!actionID || !callback){
+         return;
+      }
+
+      this.app.action(actionID, callback);
+   }
+}
