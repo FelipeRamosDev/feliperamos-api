5238077ade4ee7fee8d4149d2e15ef9c4f344f1f
FelipeRamosDev <felipe@feliperamos.info>
2025-07-18 21:48:07 -0300

    Update update.ts

--- src/routes/company/update.ts
+++ src/routes/company/update.ts
@@ -8,13 +8,19 @@ export default new Route({
    useAuth: true,
    allowedRoles: ['admin', 'master'],
    controller: async (req, res) => {
-      const { id, updates } = req.body;
+      const { id, updates } = Object(req.body);
+
+      if (!id || !updates) {
+         new ErrorResponseServerAPI('Invalid request data', 400, 'INVALID_REQUEST').send(res);
+         return;
+      }
 
       try {
          const updatedCompany = await Company.update(id, updates);
 
          if (!updatedCompany) {
             new ErrorResponseServerAPI('Company not found', 404, 'COMPANY_NOT_FOUND').send(res);
+            return;
          }
          
          res.status(200).send(updatedCompany);
