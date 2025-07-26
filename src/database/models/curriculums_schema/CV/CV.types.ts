cd63bb4ced18728e0c8d1540119846985859fef4
FelipeRamosDev <felipe@feliperamos.info>
2025-07-25 22:58:33 -0300

    [FRCV-83] Route /curriculum/:cv_id
    ## [FRCV-83] Description
    Introduces a GET /curriculum/:cv_id route for fetching a CV with all related sets, experiences, and skills. Updates the CV model to support population of related entities and adds a static getFullById method. Adjusts types to support new data structures.

--- src/database/models/curriculums_schema/CV/CV.types.ts
+++ src/database/models/curriculums_schema/CV/CV.types.ts
@@ -1,12 +1,14 @@
 import { ExperienceSetup } from "../../experiences_schema/Experience/Experience.types";
 import { SkillSetup } from "../../skills_schema/Skill/Skill.types";
+import CVSet from "../CVSet/CVSet";
 import { CVSetSetup } from "../CVSet/CVSet.types";
 
 export interface CVSetup extends CVSetSetup {
    title: string;
    is_master?: boolean;
    notes?: string;
-   cv_experiences?: ExperienceSetup[];
-   cv_skills?: SkillSetup[];
+   cv_experiences?: (ExperienceSetup | number)[];
+   cv_skills?: (SkillSetup | number)[];
    cv_owner?: number; 
+   languageSets?: CVSet[];
 }
