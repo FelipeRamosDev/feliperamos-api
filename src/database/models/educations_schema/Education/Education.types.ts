1c3e2481f3935b140d8f53b26ff71453f097985d
FelipeRamosDev <felipe@feliperamos.info>
2025-08-09 12:59:25 -0300

    Update Education.types.ts

--- src/database/models/educations_schema/Education/Education.types.ts
+++ src/database/models/educations_schema/Education/Education.types.ts
@@ -1,9 +1,6 @@
 import { EducationSetSetup } from '../EducationSet/EducationSet.types';
 
 export interface EducationSetup extends EducationSetSetup {
-   id: number;
-   created_at: Date;
-   updated_at: Date;
    institution_name: string;
    start_date: Date;
    end_date: Date;
